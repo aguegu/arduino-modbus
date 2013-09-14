434fd64d4ad649d29dfb78c777fe71336122217e
Weihong Guan <weihong.guan@gmail.com>
2013-09-15 02:20:13 +0800

    minor update

--- test/md_readbit.py
+++ test/md_readbit.py
@@ -47,32 +47,3 @@ print "[0].value: %d" % x.read_register(1, 0, 3, False)
 x.write_register(3, 0, 0, 6, False)
 print "[1].value: %d" % x.read_register(3, 0, 3, False)
 
-
-'''
-print x.read_bit(0, 2);
-
-x.write_bit(0, 1, 5)
-print x.read_bit(0, 1)
-time.sleep(0.2)
-
-x.write_bit(0, 0, 15)
-print x.read_bit(0, 1)
-
-adc = x.read_register(0, 0, 4, False)
-print "%4d\t%.2f%%" % (adc, adc / 16.0)
-
-fan = x.write_register(0, 100, 0, 6, False)
-
-x.write_string(10, "hello", 3)
-print x.read_string(10, 3, 3)
-
-x.write_string(10, "world", 3)
-print x.read_string(10, 3, 3)
-
-03050000ff008dd8
-030500000000cc28
-
-00050000ff008DEB
-000500000000CC1B
-
-'''
